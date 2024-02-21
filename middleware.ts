865222828f2ee0a52f604b45162ff85688e6ad55
Admin <admin@example.com>
2024-02-21 06:50:14 +0000

    update middleware.

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,35 @@
 import { jwtVerify } from "jose";
 import { NextRequest, NextResponse } from "next/server";
-import { UserModel } from "./lib/db/model/user";
-import dbconnect from "@foodordering/lib/db/db";
 
 export async function middleware(request: NextRequest) {
-  dbconnect();
   const { pathname } = request.nextUrl;
   const tokenCookie = request.cookies.get("token");
 
-  const { payload } = await jwtVerify(
-    tokenCookie?.value as string,
-    new TextEncoder().encode(process.env.SECRET_KEY)
-  );
-
-  const { is_admin } = payload;
-
   const adminRoutes = ["/admin/orders"];
 
-  console.log(process.env);
+  if (
+    adminRoutes.some((route) => {
+      return pathname.startsWith(route);
+    })
+  ) {
+    if (tokenCookie) {
+      try {
+        const { payload } = await jwtVerify(
+          tokenCookie?.value as string,
+          new TextEncoder().encode(process.env.SECRET_KEY)
+        );
 
-  if (adminRoutes.includes(pathname) && !is_admin) {
-    return NextResponse.redirect(`${process.env.BASE_URL}/login`);
+        const { is_admin } = payload;
+        if (is_admin) {
+          NextResponse.next();
+        }
+      } catch (error) {
+        console.log(error);
+        return NextResponse.redirect(`${process.env.BASE_URL}/login`);
+      }
+    } else {
+      return NextResponse.redirect(`${process.env.BASE_URL}/login`);
+    }
   }
+  return NextResponse.next();
 }
